af98daac120b6822111e650350092c856c31d00d
Wemerson <pwemerson45@gmail.com>
2025-12-28 17:39:37 -0300

    Primeiro commit

---
+++ src/main/java/com/springboot/CadastroDeItens/CadastroDeItensApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.CadastroDeItens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeItensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeItensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/CadastroDeItens/CadastroDeItensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.CadastroDeItens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeItensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
